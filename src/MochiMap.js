333e39358e464d7cfb6717ab17e7b6e5dc79fbf6
Chrisdigity <chrisdigity@gmail.com>
2021-04-15 14:13:37 +1000

    reconfigure MochiMap to utilise routes

--- src/MochiMap.js
+++ src/MochiMap.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Route } from 'react-router-dom';
+import { Header, Navbar, Footer } from './Layout';
+import { Home, Explorer, Block, SearchBlocks, Tx, SearchTxs } from './Routes';
 
-function App() {
+function MochiMap () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter basename='/'>
+      <div className='container'>
+        <Header headerTitle='Mochimap - Blockchain Explorer' />
+        <Navbar />
+        <main className='main'>
+          <div id='display' />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/explorer' component={Explorer} />
+          <Route exact path='/explorer/block/:id' component={Block} />
+          <Route exact path='/explorer/transaction/:id' component={Tx} />
+          <Route path='/explorer/search/blocks' component={SearchBlocks} />
+          <Route path='/explorer/search/transactions' component={SearchTxs} />
+        </main>
+        <Footer />
+      </div>
+    </HashRouter>
   );
 }
 
-export default App;
+export default MochiMap;
---
+++ src/Routes.js
@@ -0,0 +1,8 @@
+import Home from './www/index';
+import Explorer from './www/explorer/index';
+import Block from './www/explorer/block';
+import BlockSearch from './www/explorer/block/search';
+import Tx from './www/explorer/transaction';
+import TxSearch from './www/explorer/transaction/search';
+
+export { Home, Explorer, Block, BlockSearch, Tx, TxSearch };
