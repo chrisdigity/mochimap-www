5caa1c62c2aec96e4a628106e27ed352f0054a68
Chrisdigity <chrisdigity@gmail.com>
2021-04-15 16:13:58 +1000

    update index.js to reflect latest project updates

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import MochiMap from './MochiMap';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MochiMap />
   </React.StrictMode>,
   document.getElementById('root')
 );
