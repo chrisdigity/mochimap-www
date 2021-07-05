605a9f28bb3d2f9b5682baad81edf9ba9f710981
Chrisdigity <chrisdigity@gmail.com>
2021-07-05 11:28:20 +1000

    introduce material-ui framework on App
    Also introduces reorganised directory tree separating dumb components
    (as pages) and smart components (as components).

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,80 @@
-import Home from './www/index';
-import Explorer from './www/explorer/index';
-import Block from './www/explorer/block/index';
-import BlockSearch from './www/explorer/block/search';
-import Ledger from './www/explorer/ledger/index';
-import LedgerSearch from './www/explorer/ledger/search';
-import Transaction from './www/explorer/transaction';
-import TransactionSearch from './www/explorer/transaction/search';
 
+import { useMemo, useState } from 'react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Paper } from '@material-ui/core';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import ScopedCssBaseline from '@material-ui/core/ScopedCssBaseline';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+// import app-components
+import Nopage from './app/component/Nopage';
+import Header from './app/component/Header';
+import Footer from './app/component/Footer';
+// import app-pages
+import Homepage from './app/homepage';
+import BlockSearch from './app/block-search';
+import BlockBnumBhash from './app/block-#bnum-#bhash';
+import Explorer from './app/explorer';
+import LedgerRichlist from './app/ledger-richlist';
+import LedgerSearch from './app/ledger-search';
+import LedgerTypeAddress from './app/ledger-#type-#address';
+import TransactionMempool from './app/transaction-mempool';
+import TransactionSearch from './app/transaction-search';
+import TransactionTXID from './app/transaction-#txid';
+
+// describe app-routes
 export const RouteList = [
-  { path: '/haiku', route: undefined, text: 'Haiku' },
-  { path: '/explorer/transaction/search', route: TransactionSearch },
-  { path: '/explorer/transaction/:txid', route: Transaction },
-  { path: '/explorer/ledger/search', route: LedgerSearch },
-  { path: '/explorer/ledger/:type/:address', route: Ledger },
-  { path: '/explorer/block/search', route: BlockSearch },
-  { path: '/explorer/block/:bnum', route: Block },
-  { path: '/explorer', route: Explorer, text: 'Explorer' },
-  { path: '/about', text: 'What is Mochimo?' },
-  { path: '/', route: Home }
+  { path: '/', route: Homepage },
+  { path: '/block/search', route: BlockSearch },
+  { path: '/block/:bnum/:bhash', route: BlockBnumBhash },
+  { path: '/block/:bnum', route: BlockBnumBhash },
+  { path: '/explorer', route: Explorer, nav: 'Explorer' },
+  { path: '/ledger/richlist', route: LedgerRichlist, nav: 'Richlist' },
+  { path: '/ledger/search', route: LedgerSearch },
+  { path: '/ledger/:type/:address', route: LedgerTypeAddress },
+  { path: '/transaction/mempool', route: TransactionMempool, nav: 'Mempool' },
+  { path: '/transaction/search', route: TransactionSearch },
+  { path: '/transaction/:txid', route: TransactionTXID },
+  { href: 'https://mochimo.org/', nav: 'What is Mochimo?' }
 ];
+
+export default function App () {
+  const [themeType, setThemeType] = useState();
+  const theme = useMemo(() => {
+    const preferredMode = typeof themeType !== 'undefined' ? themeType
+      : useMediaQuery('(prefers-color-scheme: light)') ? 'light' : 'dark';
+    return createMuiTheme({
+      palette: {
+        type: preferredMode,
+        primary: {
+          main: '#424242'
+        },
+        secondary: {
+          main: '#0059ff'
+        }
+      }
+    });
+  }, [themeType]);
+
+  return (
+    <ScopedCssBaseline>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter basename='/'>
+          <Paper>
+            <Header routelist={RouteList} setThemeType={setThemeType} />
+            <Switch>
+              {RouteList.filter((route) => route.path).map((route, i) =>
+                <Route
+                  exact path={route.path}
+                  component={route.route}
+                  key={i}
+                />
+              )}
+              <Route component={Nopage} />
+            </Switch>
+            <Footer />
+          </Paper>
+        </BrowserRouter>
+      </ThemeProvider>
+    </ScopedCssBaseline>
+  );
+}
