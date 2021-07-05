5876406b611e0738ce96913bc93a0d85a829a87a
Chrisdigity <chrisdigity@gmail.com>
2021-07-05 11:24:16 +1000

    implement redux toolkit ApiProvider wrapping base App render

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 
-import MochiMap from './MochiMap';
-import ReactDOM from 'react-dom';
-import React from 'react';
+import { StrictMode } from 'react';
+import { render } from 'react-dom';
+import { ApiProvider } from '@reduxjs/toolkit/query/react';
+import MochimapApi from './app/service/mochimap-api';
+import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <MochiMap />
-  </React.StrictMode>,
+render(
+  <ApiProvider api={MochimapApi}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </ApiProvider>,
   document.getElementById('root')
 );
