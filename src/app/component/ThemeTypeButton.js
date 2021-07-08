38d4a92d991c11dcc0d2cce082ee5b460fb49e59
Chrisdigity <chrisdigity@gmail.com>
2021-07-08 17:11:19 +1000

    fix incorrect position of onClick and rename export to ThemeButton

--- src/app/component/ThemeTypeButton.js
+++ src/app/component/ThemeTypeButton.js
@@ -4,17 +4,15 @@ import { IconButton, Tooltip } from '@material-ui/core';
 import NightsStayIcon from '@material-ui/icons/NightsStay';
 import BrightnessHighIcon from '@material-ui/icons/BrightnessHigh';
 
-export default function ThemeTypeButton ({ setThemeType }) {
-  const { DARK, LIGHT } = { DARK: 'dark', LIGHT: 'light' };
-  const themeType = useTheme()?.palette?.type || DARK;
-  const opposingTheme = themeType === DARK ? LIGHT : DARK;
-  const handleClick = () => setThemeType(opposingTheme);
-  const Brightness = themeType === DARK ? BrightnessHighIcon : NightsStayIcon;
+export default function ThemeButton ({ switchTheme }) {
+  const { DARK } = { DARK: 'dark' };
+  const theme = useTheme()?.palette?.type || DARK;
+  const label = 'Switch Theme';
 
   return (
-    <Tooltip title='Switch Theme'>
-      <IconButton aria-label='Toggle Theme'>
-        <Brightness onClick={handleClick} />
+    <Tooltip title={label}>
+      <IconButton aria-label={label} onClick={switchTheme}>
+        {theme === DARK ? <BrightnessHighIcon /> : <NightsStayIcon />}
       </IconButton>
     </Tooltip>
   );
