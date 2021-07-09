eface5bdaad599493a5538a9128dc71c2f0aa150
Chrisdigity <chrisdigity@gmail.com>
2021-07-09 22:47:33 +1000

    update explorer page with new search form and style

--- src/app/explorer.js
+++ src/app/explorer.js
@@ -1,29 +1,108 @@
 
-import { useGetChainByLatestQuery } from './service/mochimap-api';
+import { useState } from 'react';
+import {
+  Container,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  Paper,
+  Select,
+  TextField,
+  Typography
+} from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 import { makeStyles } from '@material-ui/core/styles';
-import { Container, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
+  formContainer: {
     position: 'relative',
-    margin: '0 auto',
-    width: '80vw',
-    height: '300vh',
-    border: '0.25em solid #0059ff',
-    'white-space': 'pre'
+    height: '45vh',
+    display: 'flex',
+    'flex-direction': 'column',
+    'justify-content': 'end',
+    'align-items': 'center'
+  },
+  formTitle: {
+    'font-family': 'Nanum Brush Script',
+    'font-weight': 'bold'
+  },
+  form: {
+    'min-width': '450px',
+    display: 'flex',
+    padding: theme.spacing(4),
+    'padding-bottom': theme.spacing(2),
+    'padding-top': theme.spacing(1),
+    background: theme.palette.divider
+  },
+  grow: {
+    'flex-grow': 1
   }
 }));
 
 export default function Explorer () {
-  const { data, isError, isLoading } = useGetChainByLatestQuery();
+  const [searchType, setSearchType] = useState();
+  const [searchHint, setSearchHint] = useState('Type Search Query');
+  const handleSearchType = (event) => {
+    const { value } = event.target;
+    switch (value) {
+      case 'block': setSearchHint('Type Block Number'); break;
+      case 'transaction': setSearchHint('Type Transaction ID'); break;
+      case 'address': setSearchHint('Type WOTS+ Address'); break;
+      case 'tag': setSearchHint('Type Tagged Address'); break;
+      case 'network': setSearchHint('Type IPv4 Address'); break;
+      default: setSearchHint('Type Search Query');
+    }
+    setSearchType(value);
+  };
   const classes = useStyles();
 
   return (
-    <Container className={classes.root}>
-      <Typography>
-        {isLoading ? 'Loading...' : isError ? 'ERROR!!!'
-          : JSON.stringify(data, null, 2)}
-      </Typography>
+    <Container>
+      <form className={classes.formContainer}>
+        <Typography className={classes.formTitle} variant='h1'>
+          Mochimo Explorer
+        </Typography>
+        <Paper className={classes.form}>
+          <FormControl className={classes.selectControl}>
+            <InputLabel id='search-native-label'>Search for</InputLabel>
+            <Select
+              native
+              dir='rtl'
+              margin='normal'
+              color='secondary'
+              value={searchType}
+              onChange={handleSearchType}
+              labelId='search-native-label'
+              inputProps={{ name: searchType && 'search' }}
+            >
+              <option aria-label='all' />
+              <option value='block'>Block</option>
+              <option value='transaction'>Transaction</option>
+              <option value='address'>Address</option>
+              <option value='tag'>Tag</option>
+              <option value='network'>Node</option>
+            </Select>
+          </FormControl>
+          <TextField
+            autfocus
+            margin='normal'
+            className={classes.grow}
+            color='secondary'
+            placeholder={`${searchHint}...`}
+            InputProps={{
+              name: 'for',
+              endAdornment: (
+                <InputAdornment position='end'>
+                  <IconButton type='submit' aria-label='search'>
+                    <SearchIcon />
+                  </IconButton>
+                </InputAdornment>
+              )
+            }}
+          />
+        </Paper>
+      </form>
     </Container>
   );
 }
