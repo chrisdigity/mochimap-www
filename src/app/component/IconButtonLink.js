701c6e2845293480d6cc48bc62d92026aab11784
Chrisdigity <chrisdigity@gmail.com>
2021-07-09 22:43:17 +1000

    add IconButtonLink component for use in Header

--- src/app/component/IconButtonLink.js
+++ src/app/component/IconButtonLink.js
@@ -1,12 +1,17 @@
 
 import { IconButton, Link, Tooltip } from '@material-ui/core';
 
-export default function IconButtonLink ({ handleClick, Icon, label, path }) {
+export default function IconButtonLink ({ action, edge, Icon, label, path }) {
+  const isComponent = typeof Icon === 'function' || typeof Icon === 'object';
+  const handleClick = action;
+
   return (
     <Link href={path || '#'} onClick={handleClick}>
       <Tooltip title={label}>
-        <IconButton aria-label={label}>
-          <Icon />
+        <IconButton aria-label={label} edge={edge}>
+          {isComponent ? <Icon /> : (
+            <img src={Icon} width='24' height='24' />
+          )}
         </IconButton>
       </Tooltip>
     </Link>
