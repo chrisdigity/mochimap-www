6645399ab1ab9f7fa17cfab99002a676e8e0c5fb
Chrisdigity <chrisdigity@gmail.com>
2021-07-31 18:03:52 +1000

    implement MochimoAddress component for consistant display of addresses

---
+++ src/app/component/MochimoAddress.js
@@ -0,0 +1,19 @@
+
+import { Link } from 'react-router-dom';
+
+const isUntagged = (addr) => ['00', '42'].includes(addr.slice(0, 2));
+
+export default function MochimoAddress ({ pre, tag, addr, disableLinks }) {
+  return (
+    <>
+      {pre || ''}
+      {!tag || isUntagged(tag) ? null : disableLinks ? `τ-${tag}` : (
+        <Link to={`/explorer/ledger/tag/${tag}`}>τ-{tag}</Link>
+      )}
+      {tag && addr ? <span> • </span> : null}
+      {!addr ? null : disableLinks ? `ω+${addr}` : (
+        <Link to={`/explorer/ledger/address/${addr}`}>ω+{addr}</Link>
+      )}
+    </>
+  );
+}
