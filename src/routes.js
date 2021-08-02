8170f6057ce457ce546012270aafb3c05b20facd
Chrisdigity <chrisdigity@gmail.com>
2021-08-02 10:00:48 +1000

    enable richlist page and implement subnav array for menu list navigation

--- src/routes.js
+++ src/routes.js
@@ -1,37 +1,48 @@
 
 import ExploreIcon from '@material-ui/icons/Explore';
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
 // import app-pages
 import Homepage from './app/homepage';
-// import BlockSearch from './app/block-search';
 // import BlockBnumBhash from './app/block-#bnum-#bhash';
 import Explorer from './app/explorer';
-// import LedgerRichlist from './app/ledger-richlist';
-// import LedgerSearch from './app/ledger-search';
+import ExplorerRichlist from './app/explorer-richlist';
 import ExplorerLedgerTypeAddress from './app/explorer-ledger-#type-#address';
 // import TransactionMempool from './app/transaction-mempool';
-// import TransactionSearch from './app/transaction-search';
 // import TransactionTXID from './app/transaction-#txid';
 
-// describe app-routes
+// describe app-routes-nav-subnav
 export default [
-  { path: '/', route: Homepage },
-  // { path: '/block/search', route: BlockSearch },
+  { // route only
+    route: Homepage,
+    path: '/'
+  },
   // { path: '/block/:bnum/:bhash', route: BlockBnumBhash },
   // { path: '/block/:bnum', route: BlockBnumBhash },
-  {
+  { // route path, nav item, subnav items
     route: Explorer,
     path: '/explorer',
     Icon: ExploreIcon,
-    header: 'Explorer',
-    desc: 'Mochimo Block Explorer'
-  },
-  // { path: '/ledger/richlist', route: LedgerRichlist, text: 'Richlist' },
-  // { path: '/ledger/search', route: LedgerSearch },
-  {
+    nav: 'Explorer',
+    subnav: [
+      {
+        path: '/explorer',
+        Icon: ExploreIcon,
+        primary: 'Mochimo Explorer Search',
+        secondary: 'Search blocks, transactions, etc.'
+      }, {
+        path: '/explorer/richlist',
+        Icon: AccountBalanceIcon,
+        primary: 'Mochimo Richlist',
+        secondary: 'Latest Ledger Rankings'
+      }
+    ]
+  }, { // route only
+    route: ExplorerRichlist,
+    path: '/explorer/richlist'
+  }, { // route only
     route: ExplorerLedgerTypeAddress,
     path: '/explorer/ledger/:type/:address'
   } //,
   // { path: '/transaction/mempool', route: TransactionMempool, text: 'Mempool' },
-  // { path: '/transaction/search', route: TransactionSearch },
   // { path: '/transaction/:txid', route: TransactionTXID }
 ];
