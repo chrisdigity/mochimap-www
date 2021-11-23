aead55eda6edb743e6a9c874cb2b5e5aa484d12b
Chrisdigity <chrisdigity@gmail.com>
2021-11-23 22:17:51 +1000

    update routes for latest page changes

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
 
 import ExploreIcon from '@material-ui/icons/Explore';
 import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
+import ViewListIcon from '@material-ui/icons/ViewList';
 // import app-pages
 import Homepage from './app/homepage';
-// import BlockBnumBhash from './app/block-#bnum-#bhash';
 import Explorer from './app/explorer';
+import ExplorerBlock from './app/explorer-block-#bnum-#bhash';
 import ExplorerRichlist from './app/explorer-richlist';
 import ExplorerLedgerTypeAddress from './app/explorer-ledger-#type-#address';
 // import TransactionMempool from './app/transaction-mempool';
@@ -16,8 +17,6 @@ export default [
     route: Homepage,
     path: '/'
   },
-  // { path: '/block/:bnum/:bhash', route: BlockBnumBhash },
-  // { path: '/block/:bnum', route: BlockBnumBhash },
   { // route path, nav item, subnav items
     route: Explorer,
     path: '/explorer',
@@ -34,14 +33,30 @@ export default [
         Icon: AccountBalanceIcon,
         primary: 'Mochimo Richlist',
         secondary: 'Latest Ledger Rankings'
+      }, {
+        path: '/explorer?search=transaction',
+        Icon: ViewListIcon,
+        primary: 'Mochimo Transactions',
+        secondary: 'Latest Transactions results'
+      }, {
+        path: '/explorer?search=blockchain',
+        Icon: ViewListIcon,
+        primary: 'Mochimo Blocks',
+        secondary: 'Latest Blockchain results'
       }
     ]
-  }, { // route only
-    route: ExplorerRichlist,
-    path: '/explorer/richlist'
+  }, {
+    route: ExplorerBlock,
+    path: '/explorer/block/:bnum/:bhash'
+  }, {
+    route: ExplorerBlock,
+    path: '/explorer/block/:bnum'
   }, { // route only
     route: ExplorerLedgerTypeAddress,
     path: '/explorer/ledger/:type/:address'
+  }, { // route only
+    route: ExplorerRichlist,
+    path: '/explorer/richlist'
   } //,
   // { path: '/transaction/mempool', route: TransactionMempool, text: 'Mempool' },
   // { path: '/transaction/:txid', route: TransactionTXID }
